2f3f14a2a269abc675962a8df39cf423f42e513f
Mayuriyupe23 <mayuriyupe23@navgurukul.org>
2024-10-09 12:05:32 +0530

    after changes

--- rentals/www/hello.py
+++ rentals/www/hello.py
@@ -1,9 +1,9 @@
-import frappe
-def get_context(context):
-    context.my_secret_emoji="🤭"
+# import frappe
+# def get_context(context):
+#     context.my_secret_emoji="🤭"
     
     
     
     
-    url?isLoggedIn=true&name="puran'&age="18" 
+    # url?isLoggedIn=true&name="puran'&age="18" 
     #isko click hone ke bad ek form aana chahiyeor aur automatically value form pe set hony chahiye
